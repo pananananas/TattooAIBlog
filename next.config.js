4f9dba9b31d32803990699ee435c6c70c157dfc7
Eryk Wójcik <drumpixtv@gmail.com>
2024-11-24 21:07:11 +0100

    updated main page with images

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/a/oxjj5brc17/**",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
