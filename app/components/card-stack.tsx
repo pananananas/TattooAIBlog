512c3e44ca50c3485724e18adf316677c3a5de69
Eryk Wójcik <drumpixtv@gmail.com>
2024-11-26 14:54:44 +0100

    fixed size of card stack

--- app/components/card-stack.tsx
+++ app/components/card-stack.tsx
@@ -381,7 +381,7 @@ const CardStack: React.FC<CardStackProps> = ({ images = [], id }) => {
   }, [activeIndex, images.length]);
 
   return (
-    <div ref={parentRef} className="relative h-[18rem] w-[12rem]">
+    <div ref={parentRef} className="relative h-[18rem] w-[10rem]">
       {/* {(!hasScrolled || arrowClicked) && (
         <>
           {activeIndex > 0 && (
